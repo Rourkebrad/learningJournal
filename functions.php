0d717ecafbae95cdf03dc7f08e375f5951285fa4
Rourkebrad <37929040+Rourkebrad@users.noreply.github.com>
2020-03-04 21:04:28 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,10 +7,12 @@ function entriesForIndex()
 {
   include('inc/connection.php');
   try{
-  $sql = "SELECT entries.title, entries.date, entries.id, tags.tag FROM 'entries'
-  JOIN entries_tags ON entries.id = entries_tags.entries_id JOIN tags ON entries_tags.tag_id = tags.tag_id ORDER BY date ASC";
+    $sql = "SELECT title, date, id FROM 'entries'
+     ORDER BY date ASC";
+    /*$sql = "SELECT title, date, id, tags.tag FROM 'entries'
+    JOIN entries_tags ON entries.id = entries_tags.entries_id JOIN tags ON entries_tags.tag_id = tags.tag_id ORDER BY date ASC";*/
   $results = $db->query($sql);
-  //$results = $db->query("INSERT INTO entries_tags (entries_id, tag_id) VALUES ($id, $tag_id)");
+
 
   }
   catch(Exception $e)
@@ -22,23 +24,23 @@ function entriesForIndex()
   return $catalog;
 }
 
-function add_entry($title, $date, $time_spent, $learned, $resources, $tag)
+function add_entry($title, $date, $time_spent, $learned, $resources)
 {
   include('inc/connection.php');
   $sql = "INSERT INTO entries(title, date, time_spent, learned, resources) VALUES(?, ?, ?, ?, ?)";
-  $sql2 = "INSERT INTO tags(tag) VALUES (?)";
+  //$sql2 = "INSERT INTO tags(tag) VALUES (?)";
   try
   {
     $results = $db->prepare($sql);
-    $results2 = $db->prepare($sql2);
+  //  $results2 = $db->prepare($sql2);
     $results->bindValue(1, $title, PDO::PARAM_STR);
     $results->bindValue(2, $date, PDO::PARAM_STR);
     $results->bindValue(3, $time_spent, PDO::PARAM_INT);
     $results->bindValue(4, $learned, PDO::PARAM_STR);
     $results->bindValue(5, $resources, PDO::PARAM_STR);
-    $results2->bindValue(1, $tag, PDO::PARAM_STR);
+  //  $results2->bindValue(1, $tag, PDO::PARAM_STR);
     $results->execute();
-    $results2->execute();
+  //  $results2->execute();
   }
   catch(Exception $e)
   {
